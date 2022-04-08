def66a775372e37a364d5314441816b8ae0ab784
Mario Rovito <alessandro.r14@gmail.com>
2022-04-08 13:09:13 +0200

    add code and component header and main

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    header
+  </div>
+</template>
+
+<script>
+export default {
+name: 'headerComponent',
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    main
+  </div>
+</template>
+
+<script>
+export default {
+name: 'mainComponent',
+}
+</script>
+
+<style>
+
+</style>
