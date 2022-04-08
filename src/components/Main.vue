20e90cc245153878c64fcb370440624f2fea6ae8
Mario Rovito <alessandro.r14@gmail.com>
2022-04-08 16:29:19 +0200

    add comp songItem

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,15 +1,29 @@
 <template>
   <div class="container">
-    main
+    <div class="row">
+      <div v-if="element.length > 0 " class="col col-md-6 col-xl-4">
+        <SongItem :singleCd="element[0]" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import SongItem from '@/components/SongItem.vue'
+
 export default {
 name: 'mainComponent',
+data(){
+  return{
+    element:[]
+  }
+},
 props:{
   album: String
+},
+components:{
+  SongItem
 },
 mounted(){
   this.loadAlbum();
@@ -18,7 +32,11 @@ methods:{
 loadAlbum(){
   axios.get(this.album).then(
     (response)=>{
-      console.log(response);
+      //console.log(response);
+      if (response.status === 200){
+        this.element = response.data.response;
+        console.log(this.element[0]);
+      }
     }
   )
 }
---
+++ src/components/SongItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <img :src="singleCd.poster" :alt="singleCd.genre"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "songItem",
+  props: {
+    singleCd: Object,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
