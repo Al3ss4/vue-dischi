d8720b7d79035bbd9990a39aae218b28cb201e76
Mario Rovito <alessandro.r14@gmail.com>
2022-04-11 13:34:50 +0200

    add methods in searchsong

--- src/components/SearchSong.vue
+++ src/components/SearchSong.vue
@@ -1,13 +1,25 @@
 <template>
   <div>
-      <input type="text" placeholder="Search"/>
-      <button >Search</button>
+      <input type="text" placeholder="Search"
+      v-model.trim="genreSong"
+      @keyup.enter="search" />
+      <button @click.prevent="search">Search</button>
   </div>
 </template>
 
 <script>
 export default {
-name:'SearchSong'
+name:'SearchSong',
+data(){
+    return{
+        genreSong:''
+    }
+},
+methods:{
+    search(){
+        this.$emit('SearchSong', this.genreSong);
+    }
+}
 }
 </script>
 
